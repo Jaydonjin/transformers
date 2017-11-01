bd07e1a07e628cc64921bf35169d35e3a67f45fb
DAE <DAE@newegg.com>
2017-11-01 01:24:20 -0700

    init

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Hello from '@/components/Hello'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    }
+  ];
+
+export default new VueRouter({
+  routes: routes
+})
+
